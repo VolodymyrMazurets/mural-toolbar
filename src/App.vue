afbe61f6afbd06648382431515d5bb7c2a4addbb
Volodymyr Mazurets <volodymyrmazureets@gmail.com>
2021-07-29 17:22:00 +0300

    ✨ move to position feature

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <template>
   <div class="App">
-    <div class="App__area" ref="container" @click="handleContainerClick">Draggable zone. Click on toolbar to turn on draggable mode. To dissable it click on empty space in this container</div>
-    <div class="App__toolbar" @click="handleToolbarClick">
+    <div class="App__area" ref="container" @click="handleContainerClick">
+      Draggable zone. Click on blue square to turn on draggable mode. To dissable it
+      click on empty space in this container
+      <div
+        ref="firstArea"
+        class="App__area-first"
+        @click.stop="handleToolbarClick('first')"
+      ></div>
+      <div
+        ref="secondArea"
+        class="App__area-second"
+        @click.stop="handleToolbarClick('second')"
+      ></div>
+    </div>
+    <div ref="toolbar" class="App__toolbar">
       <div
         v-for="item in [1, 2, 3, 4]"
         :key="item"
@@ -30,11 +43,37 @@ export default {
   components: {},
   mounted() {},
   methods: {
-    handleToolbarClick() {
-      const { container, item1, item2, item3, item4 } = this.$refs;
+    handleToolbarClick(position) {
+      const {
+        container,
+        item1,
+        item2,
+        item3,
+        item4,
+        firstArea,
+        secondArea,
+        toolbar,
+      } = this.$refs;
+      const firstRect = firstArea.getBoundingClientRect();
+      const secondRect = secondArea.getBoundingClientRect();
+      const toolbarRect = toolbar.getBoundingClientRect();
+      const leftOffset =
+        position === "first"
+          ? firstRect.left - toolbarRect.left
+          : secondRect.left - toolbarRect.left;
+      const topOffset =
+        position === "first"
+          ? firstRect.top - toolbarRect.top
+          : secondRect.top - toolbarRect.top;
+
+      position === "first"
+        ? console.log(firstRect, toolbarRect)
+        : console.log(secondRect, toolbarRect);
+
       if (!this.draggable) {
         gsap.to([item1[0], item2[0], item3[0], item4[0]], {
-          y: "-=300",
+          y: topOffset,
+          x: leftOffset,
           backgroundColor: "red",
           duration: 0.5,
           ease: "power2.inOut",
@@ -43,12 +82,8 @@ export default {
             this.draggable = Draggable.create([item1, item2, item3, item4], {
               bounds: container,
               throwProps: true,
-              onClick: function() {
-                console.log("clicked");
-              },
-              onDragEnd: function() {
-                console.log("drag ended");
-              },
+              onClick() {},
+              onDragEnd() {},
             });
           },
         });
@@ -92,6 +127,7 @@ export default {
     margin: auto;
     background-color: rgb(218, 218, 218);
     border-radius: 4px;
+    position: relative;
   }
   &__toolbar {
     border-radius: 8px;
@@ -114,6 +150,22 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
+  }
+  &__area-first,
+  &__area-second {
+    position: absolute;
+    top: 50%;
+    width: 200px;
+    height: 200px;
+    background-color: rgb(142, 202, 230);
+  }
+  &__area-first {
+    left: 10%;
+    transform: translateY(-50%);
+  }
+  &__area-second {
+    right: 20%;
+    top: 10%;
   }
 }
 </style>
